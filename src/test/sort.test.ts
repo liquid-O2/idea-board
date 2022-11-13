094e2d3ec9eb93980aac21bd133233183fabd0e8
Arunava <this.is.arunava.b@gmail.com>
2022-11-13 23:23:29 +0530

    cleaned up the test a tad bit

--- src/test/sort.test.ts
+++ src/test/sort.test.ts
@@ -1,131 +1,40 @@
+/* eslint-disable prettier/prettier */
 import { expect, test } from '@jest/globals'
 import { sortArray } from '../utils/sort'
 import { IdeasType } from '../types'
 
-const testArray: IdeasType[] = [
-  {
-    id: '1',
-    title: 'Alpha',
-    text: 'Lorem ipsum dolor sit amet',
-    time: 12221,
-    updated: false,
-  },
-  {
-    id: '3',
-    title: 'Gamma',
-    text: 'Lorem ipsum dolor sit amet',
-    time: 12230,
-    updated: false,
-  },
-  {
-    id: '2',
-    title: 'Beta',
-    text: 'Lorem ipsum dolor sit amet',
-    time: 12223,
-    updated: false,
-  },
-]
+const alpha = {
+  id: '1',
+  title: 'Alpha',
+  text: 'Lorem ipsum dolor sit amet',
+  time: 12221,
+  updated: false,
+}
 
-test('Takes an array of objects and sorts it by the title in alphabetical order', () => {
-  expect(sortArray(testArray, 'title', false)).toStrictEqual([
-    {
-      id: '1',
-      title: 'Alpha',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12221,
-      updated: false,
-    },
-    {
-      id: '2',
-      title: 'Beta',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12223,
-      updated: false,
-    },
-    {
-      id: '3',
-      title: 'Gamma',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12230,
-      updated: false,
-    },
-  ])
-})
+const gamma = {
+  id: '3',
+  title: 'Gamma',
+  text: 'Lorem ipsum dolor sit amet',
+  time: 12230,
+  updated: false,
+}
 
-test('Takes an array of objects and sorts it by the title in descending order', () => {
-  expect(sortArray(testArray, 'title', true)).toStrictEqual([
-    {
-      id: '3',
-      title: 'Gamma',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12230,
-      updated: false,
-    },
-    {
-      id: '2',
-      title: 'Beta',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12223,
-      updated: false,
-    },
-    {
-      id: '1',
-      title: 'Alpha',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12221,
-      updated: false,
-    },
-  ])
-})
+const beta = {
+  id: '2',
+  title: 'Beta',
+  text: 'Lorem ipsum dolor sit amet',
+  time: 12223,
+  updated: false,
+}
 
-test('Takes an array of objects and sorts it by the time in ascending order', () => {
-  expect(sortArray(testArray, 'time', false)).toStrictEqual([
-    {
-      id: '1',
-      title: 'Alpha',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12221,
-      updated: false,
-    },
-    {
-      id: '2',
-      title: 'Beta',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12223,
-      updated: false,
-    },
-    {
-      id: '3',
-      title: 'Gamma',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12230,
-      updated: false,
-    },
-  ])
-})
+const shuffledIdeas: IdeasType[] = [gamma, alpha, beta]
 
-test('Takes an array of objects and sorts it by the time in descending order', () => {
-  expect(sortArray(testArray, 'time', true)).toStrictEqual([
-    {
-      id: '3',
-      title: 'Gamma',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12230,
-      updated: false,
-    },
-    {
-      id: '2',
-      title: 'Beta',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12223,
-      updated: false,
-    },
-    {
-      id: '1',
-      title: 'Alpha',
-      text: 'Lorem ipsum dolor sit amet',
-      time: 12221,
-      updated: false,
-    },
-  ])
+test.each`
+  property   | isDescending | expected
+  ${'title'} | ${false}     | ${[alpha, beta, gamma]}
+  ${'title'} | ${true}     | ${[gamma, beta, alpha]}
+  ${'time'} | ${false}     | ${[alpha, beta, gamma]}
+  ${'time'} | ${true}     | ${[gamma, beta, alpha]}
+`('should sort by $property and isDecending=$isDescending', ({ property, isDescending, expected }) => {
+  expect(sortArray(shuffledIdeas, property as string, isDescending as boolean)).toStrictEqual(expected)
 })
