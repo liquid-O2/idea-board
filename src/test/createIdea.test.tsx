b5ed8cef6b89c84063f1f705c6b36987e1bce460
Arunava <this.is.arunava.b@gmail.com>
2022-11-18 22:07:02 +0530

    wrapped the test in a "describe"

--- src/test/createIdea.test.tsx
+++ src/test/createIdea.test.tsx
@@ -1,30 +1,32 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { expect, test } from '@jest/globals'
+import { describe, expect, test } from '@jest/globals'
 
 import App from '../App'
 
-test('Create an idea and test if it gets displayed', async () => {
-  //
+describe('Create Idea', () => {
+  test('if the idea is displayed or not', async () => {
+    //
 
-  render(<App />)
+    render(<App />)
 
-  //
+    //
 
-  const user = userEvent.setup()
+    const user = userEvent.setup()
 
-  //
+    //
 
-  await user.click(screen.getByText('Capture and save your ideas'))
-  await user.type(screen.getByPlaceholderText('Title'), 'Idea One')
-  await user.type(
-    screen.getByPlaceholderText('Enter the description of your idea'),
-    'Description of Idea One'
-  )
-  await user.click(screen.getByText('Save Idea'))
+    await user.click(screen.getByText('Capture and save your ideas'))
+    await user.type(screen.getByPlaceholderText('Title'), 'Idea One')
+    await user.type(
+      screen.getByPlaceholderText('Enter the description of your idea'),
+      'Description of Idea One'
+    )
+    await user.click(screen.getByText('Save Idea'))
 
-  //
+    //
 
-  expect(screen.getByTestId('card-title').innerHTML).toBe('Idea One')
-  expect(screen.getByTestId('card-text').innerHTML).toBe('Description of Idea One')
+    expect(screen.getByTestId('card-title').innerHTML).toBe('Idea One')
+    expect(screen.getByTestId('card-text').innerHTML).toBe('Description of Idea One')
+  })
 })
